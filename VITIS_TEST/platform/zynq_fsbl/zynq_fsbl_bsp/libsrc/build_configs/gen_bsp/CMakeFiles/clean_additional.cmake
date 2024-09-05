afe27898d4cc8dac957eef4dc551c38ab2450814
soerj21-SDU <soerj21@student.sdu.dk>
2024-09-05 14:49:31 +0200

    Test af vitis projekt

---
+++ VITIS_TEST/platform/zynq_fsbl/zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,19 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+  file(REMOVE_RECURSE
+  "C:\\Software\\GitHub\\Software_Zynq-Based_Master_Controller\\VITIS_TEST\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\diskio.h"
+  "C:\\Software\\GitHub\\Software_Zynq-Based_Master_Controller\\VITIS_TEST\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\ff.h"
+  "C:\\Software\\GitHub\\Software_Zynq-Based_Master_Controller\\VITIS_TEST\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\ffconf.h"
+  "C:\\Software\\GitHub\\Software_Zynq-Based_Master_Controller\\VITIS_TEST\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\sleep.h"
+  "C:\\Software\\GitHub\\Software_Zynq-Based_Master_Controller\\VITIS_TEST\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilffs.h"
+  "C:\\Software\\GitHub\\Software_Zynq-Based_Master_Controller\\VITIS_TEST\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilffs_config.h"
+  "C:\\Software\\GitHub\\Software_Zynq-Based_Master_Controller\\VITIS_TEST\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilrsa.h"
+  "C:\\Software\\GitHub\\Software_Zynq-Based_Master_Controller\\VITIS_TEST\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xiltimer.h"
+  "C:\\Software\\GitHub\\Software_Zynq-Based_Master_Controller\\VITIS_TEST\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xtimer_config.h"
+  "C:\\Software\\GitHub\\Software_Zynq-Based_Master_Controller\\VITIS_TEST\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxilffs.a"
+  "C:\\Software\\GitHub\\Software_Zynq-Based_Master_Controller\\VITIS_TEST\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxilrsa.a"
+  "C:\\Software\\GitHub\\Software_Zynq-Based_Master_Controller\\VITIS_TEST\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxiltimer.a"
+  )
+endif()
