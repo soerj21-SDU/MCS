6f5d2b38904f0c8141897220ee8865bb19b372c8
soerj21-SDU <soerj21@student.sdu.dk>
2024-09-26 13:24:56 +0200

    UART test assumed to be working. UART read on serial COM port

---
+++ Drivers/UART/Vitis/platform/zynq_fsbl/zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/libsrc/standalone/src/CMakeFiles/xilstandalone.dir/arm/common/gcc/_exit.c.obj.d
@@ -0,0 +1,10 @@
+libsrc/standalone/src/CMakeFiles/xilstandalone.dir/arm/common/gcc/_exit.c.obj: \
+ C:/Git/GitHub/MCS/Drivers/UART/Vitis/platform/zynq_fsbl/zynq_fsbl_bsp/libsrc/standalone/src/arm/common/gcc/_exit.c \
+ C:/Git/GitHub/MCS/Drivers/UART/Vitis/platform/zynq_fsbl/zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/include/xil_types.h \
+ C:/Git/GitHub/MCS/Drivers/UART/Vitis/platform/zynq_fsbl/zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/include/bspconfig.h \
+ C:/Git/GitHub/MCS/Drivers/UART/Vitis/platform/zynq_fsbl/zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/include/xmem_config.h \
+ C:/Git/GitHub/MCS/Drivers/UART/Vitis/platform/zynq_fsbl/zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/include/xparameters_ps.h
+C:/Git/GitHub/MCS/Drivers/UART/Vitis/platform/zynq_fsbl/zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/include/xil_types.h:
+C:/Git/GitHub/MCS/Drivers/UART/Vitis/platform/zynq_fsbl/zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/include/bspconfig.h:
+C:/Git/GitHub/MCS/Drivers/UART/Vitis/platform/zynq_fsbl/zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/include/xmem_config.h:
+C:/Git/GitHub/MCS/Drivers/UART/Vitis/platform/zynq_fsbl/zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/include/xparameters_ps.h:
