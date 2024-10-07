44a55135774b5476fe2092664a4627261df1a7a9
soerj21-SDU <soerj21@student.sdu.dk>
2024-10-07 13:15:21 +0200

    Working on interrupts 071024

---
+++ Drivers/INTR_WRPP/Vitis/GPIO_PS_TEST/src/helloworld.c
@@ -0,0 +1,59 @@
+
+
+#include "xgpiops.h"
+#include "xparameters.h"
+#include "xil_exception.h"
+#include <xil_printf.h>
+#include "sleep.h"
+
+// Define the pin and bank
+#define MIO_BTN 51
+#define MIO_LED 7 
+
+
+#define LED_PS_ADDRESS XPAR_XGPIOPS_0_BASEADDR
+
+// GPIO instance
+XGpioPs GpioPS;
+
+int carrier;
+
+// void Interrupt_BTN_51(){
+
+//     XGpioPs_IntrClear()
+// }
+
+int main() {
+    print("Starting program\n\r");
+    int Status;
+    print("Initialaizing GPIOPS_config\n\r");
+
+    XGpioPs_Config *ConfigPtrPS;
+    
+    ConfigPtrPS = XGpioPs_LookupConfig(LED_PS_ADDRESS);
+	XGpioPs_CfgInitialize( &GpioPS, ConfigPtrPS, ConfigPtrPS->BaseAddr);
+
+
+    print("LED_TEST\n\r");
+    XGpioPs_SetDirectionPin(&GpioPS, MIO_LED, 1); // 1 = output. 
+    XGpioPs_SetOutputEnablePin(&GpioPS, MIO_LED, 1);
+    XGpioPs_WritePin(&GpioPS, MIO_LED, 0x1);
+    sleep(1);
+    XGpioPs_WritePin(&GpioPS, MIO_LED, 0x0);
+
+    print("Initialaizing pin MIO51");
+    
+    XGpioPs_SetDirectionPin(&GpioPS, MIO_BTN, 0); // 0 = output. 
+   
+    while (1) {
+        print("In main\n\r");
+        sleep(1);
+        carrier = XGpioPs_ReadPin(&GpioPS, MIO_BTN);
+        XGpioPs_WritePin(&GpioPS, MIO_LED, 0x0);
+        if(carrier == 1){
+            XGpioPs_WritePin(&GpioPS, MIO_LED, 0x1);
+        }
+    }
+
+    return 0;
+}
