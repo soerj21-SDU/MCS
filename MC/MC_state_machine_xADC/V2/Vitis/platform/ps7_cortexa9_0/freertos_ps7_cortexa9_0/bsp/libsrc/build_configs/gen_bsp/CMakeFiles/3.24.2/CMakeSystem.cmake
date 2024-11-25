eebeb670b0cae7771b82df59a9297108e9bebc78
soerj21-SDU <soerj21@student.sdu.dk>
2024-11-25 14:54:27 +0100

    V2 added with LVS_measurement_task.

---
+++ MC/MC_state_machine_xADC/V2/Vitis/platform/ps7_cortexa9_0/freertos_ps7_cortexa9_0/bsp/libsrc/build_configs/gen_bsp/CMakeFiles/3.24.2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Windows-10.0.22631")
+set(CMAKE_HOST_SYSTEM_NAME "Windows")
+set(CMAKE_HOST_SYSTEM_VERSION "10.0.22631")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
+
+include("C:/Git/GitHub/MCS/MC/MC_state_machine_xADC/V2/Vitis/platform/ps7_cortexa9_0/freertos_ps7_cortexa9_0/bsp/cortexa9_toolchain.cmake")
+
+set(CMAKE_SYSTEM "FreeRTOS")
+set(CMAKE_SYSTEM_NAME "FreeRTOS")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "cortexa9")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ MC/MC_state_machine_xADC/V2/Vitis/platform/zynq_fsbl/zynq_fsbl_bsp/libsrc/build_configs/gen_bsp/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,19 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "")
+  file(REMOVE_RECURSE
+  "C:\\Git\\GitHub\\MCS\\MC\\MC_state_machine_xADC\\V2\\Vitis\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\diskio.h"
+  "C:\\Git\\GitHub\\MCS\\MC\\MC_state_machine_xADC\\V2\\Vitis\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\ff.h"
+  "C:\\Git\\GitHub\\MCS\\MC\\MC_state_machine_xADC\\V2\\Vitis\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\ffconf.h"
+  "C:\\Git\\GitHub\\MCS\\MC\\MC_state_machine_xADC\\V2\\Vitis\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\sleep.h"
+  "C:\\Git\\GitHub\\MCS\\MC\\MC_state_machine_xADC\\V2\\Vitis\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilffs.h"
+  "C:\\Git\\GitHub\\MCS\\MC\\MC_state_machine_xADC\\V2\\Vitis\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilffs_config.h"
+  "C:\\Git\\GitHub\\MCS\\MC\\MC_state_machine_xADC\\V2\\Vitis\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xilrsa.h"
+  "C:\\Git\\GitHub\\MCS\\MC\\MC_state_machine_xADC\\V2\\Vitis\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xiltimer.h"
+  "C:\\Git\\GitHub\\MCS\\MC\\MC_state_machine_xADC\\V2\\Vitis\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\include\\xtimer_config.h"
+  "C:\\Git\\GitHub\\MCS\\MC\\MC_state_machine_xADC\\V2\\Vitis\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxilffs.a"
+  "C:\\Git\\GitHub\\MCS\\MC\\MC_state_machine_xADC\\V2\\Vitis\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxilrsa.a"
+  "C:\\Git\\GitHub\\MCS\\MC\\MC_state_machine_xADC\\V2\\Vitis\\platform\\zynq_fsbl\\zynq_fsbl_bsp\\lib\\libxiltimer.a"
+  )
+endif()
