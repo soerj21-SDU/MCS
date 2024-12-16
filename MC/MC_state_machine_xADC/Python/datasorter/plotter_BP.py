a14952ff9b21c0cd18caf80f2ab22a9d9f01440b
soerj21-SDU <soerj21@student.sdu.dk>
2024-12-16 22:33:53 +0100

    Sensor data analysis complete for now. BP data still questionable.

--- MC/MC_state_machine_xADC/Python/datasorter/plotter_BP.py
+++ MC/MC_state_machine_xADC/Python/datasorter/plotter_BP.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import os
 
 # txt file paths for brake sensors
-brake_txt_file_path = os.path.join(os.path.dirname(__file__), 'BP_V1.txt')
+brake_txt_file_path = os.path.join(os.path.dirname(__file__), 'BP_V2.txt')
 
 # Extracting data from the txt files for both brake sensors
 brake0_time = extract_elapsed_time(brake_txt_file_path)
@@ -28,23 +28,27 @@ brake1_time = create_linspace(brake1_time, brake1_voltage)
 print("New Brake1 time: " + str(len(brake1_time)))
 
 # csv file paths for brake sensors
-brake_csv_file_path = 'C:\Git\GitHub\MCS\MC\MC_state_machine_xADC\Python\datasorter\LAB 03-12-24\BP_V2.csv'
+brake_csv_file_path_C1 = 'C:\Git\GitHub\MCS\MC\MC_state_machine_xADC\Python\datasorter\LAB 16-12-24\BP_V21.CSV'
+brake_csv_file_path_C2 = 'C:\Git\GitHub\MCS\MC\MC_state_machine_xADC\Python\datasorter\LAB 16-12-24\BP_V22.CSV'
 
-time = 'x-axis'
-BP0_y = 'BP0'
-BP1_y = 'BP1'
+time = 'time'
+BP0_y = 'C1'
+BP1_y = 'C2'
 
 # Reading and scaling data from the csv files for both brake sensors
-brake0_x, brake0_y = read_csv(brake_csv_file_path, time, BP0_y)
+brake0_x, brake0_y = read_csv(brake_csv_file_path_C1, time, BP0_y)
 # brake0_x, brake0_y = csv_scaler(brake0_x, brake0_y, brake0_time)
 
-brake1_x, brake1_y = read_csv(brake_csv_file_path, time, BP1_y)
+brake1_x, brake1_y = read_csv(brake_csv_file_path_C2, time, BP1_y)
 # brake1_x, brake1_y = csv_scaler(brake1_x, brake1_y, brake1_time)
 
 # Adjusting the x values
-adder = 1.16
-brake0_x = [value + adder for value in brake0_x]
-brake1_x = [value + adder for value in brake1_x]
+brake0_x_adder = 1.5+(25.61-13.27)+0.20
+brake1_x_adder = 0
+for i in range(len(brake0_x)):
+    brake0_x[i] = brake0_x[i] + brake0_x_adder
+for i in range(len(brake1_x)):
+    brake1_x[i] = brake1_x[i] + brake1_x_adder
 
 # Comparing datasets
 differences0, common_x0 = compare_datasets(brake0_x, brake0_y, brake0_time, brake0_voltage)
@@ -59,7 +63,7 @@ ax_bp0_1.plot(brake0_time, brake0_voltage, marker='o', linestyle='-', label='BP0
 ax_bp0_1.set_title('BP0 Sensor Voltage over Time scope vs ADC')
 ax_bp0_1.set_xlabel('Elapsed Time [seconds]')
 ax_bp0_1.set_ylabel('BP0 Sensor Voltage [V]')
-ax_bp0_1.set_xlim([0, 10])
+ax_bp0_1.set_xlim([12, 16])
 ax_bp0_1.legend()
 ax_bp0_1.grid(True)
 
@@ -68,10 +72,14 @@ ax_bp0_2.plot(common_x0, differences0, marker='o', linestyle='-', label='Differe
 ax_bp0_2.set_title('Differences Between BP0 Datasets')
 ax_bp0_2.set_xlabel('Elapsed Time [seconds]')
 ax_bp0_2.set_ylabel('Voltage Difference [V]')
-ax_bp0_2.set_xlim([0, 10])
-ax_bp0_2.set_ylim([-.5, .5])
+ax_bp0_2.set_xlim([12, 16])
+ax_bp0_2.set_ylim([-.2, .2])
 ax_bp0_2.legend()
 ax_bp0_2.grid(True)
+
+print(differences0[29:38+1])
+print("Average: " + str(sum(differences0[29:38+1])/len(differences0[29:38+1])))
+
 
 plt.tight_layout()
 plt.show(block=False)
@@ -85,7 +93,7 @@ ax_bp1_1.plot(brake1_time, brake1_voltage, marker='o', linestyle='-', label='BP1
 ax_bp1_1.set_title('BP1 Sensor Voltage over Time scope vs ADC')
 ax_bp1_1.set_xlabel('Elapsed Time [seconds]')
 ax_bp1_1.set_ylabel('BP1 Sensor Voltage [V]')
-ax_bp1_1.set_xlim([0, 10])
+# ax_bp1_1.set_xlim([0, 10])
 ax_bp1_1.legend()
 ax_bp1_1.grid(True)
 
@@ -94,8 +102,8 @@ ax_bp1_2.plot(common_x1, differences1, marker='o', linestyle='-', label='Differe
 ax_bp1_2.set_title('Differences Between BP1 Datasets')
 ax_bp1_2.set_xlabel('Elapsed Time [seconds]')
 ax_bp1_2.set_ylabel('Voltage Difference [V]')
-ax_bp1_2.set_xlim([0, 10])
-ax_bp1_2.set_ylim([-.5, .5])
+# ax_bp1_2.set_xlim([0, 10])
+# ax_bp1_2.set_ylim([-.5, .5])
 ax_bp1_2.legend()
 ax_bp1_2.grid(True)
 
@@ -103,31 +111,31 @@ plt.tight_layout()
 plt.show()
 
 
-BP0_value = extract_BP0_sensor(brake_txt_file_path)
+# BP0_value = extract_BP0_sensor(brake_txt_file_path)
 
 
-plt.plot(BP0_value)
-plt.xlabel('Index')
-plt.ylabel('Sensor Value')
-plt.title('Sensor Values Over Time BP0')
-plt.show()
+# plt.plot(BP0_value)
+# plt.xlabel('Index')
+# plt.ylabel('Sensor Value')
+# plt.title('Sensor Values Over Time BP0')
+# plt.show()
 
-max_value = max(BP0_value)
-min_value = min(BP0_value)
+# max_value = max(BP0_value)
+# min_value = min(BP0_value)
 
-print(f"Maximum value: {max_value}")
-print(f"Minimum value: {min_value}")
+# print(f"Maximum value: {max_value}")
+# print(f"Minimum value: {min_value}")
 
-BP1_value = extract_BP1_sensor(brake_txt_file_path)
-plt.plot(BP1_value)
-plt.xlabel('Index')
-plt.ylabel('Sensor Value')
-plt.title('Sensor Values Over Time BP1')
-plt.show()
+# BP1_value = extract_BP1_sensor(brake_txt_file_path)
+# plt.plot(BP1_value)
+# plt.xlabel('Index')
+# plt.ylabel('Sensor Value')
+# plt.title('Sensor Values Over Time BP1')
+# plt.show()
 
-max_value = max(BP1_value)
-min_value = min(BP1_value)
+# max_value = max(BP1_value)
+# min_value = min(BP1_value)
 
-print(f"Maximum value: {max_value}")
-print(f"Minimum value: {min_value}")
+# print(f"Maximum value: {max_value}")
+# print(f"Minimum value: {min_value}")
 
--- MC/MC_state_machine_xADC/Python/datasorter/plotter_TP.py
+++ MC/MC_state_machine_xADC/Python/datasorter/plotter_TP.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import os
 
 # txt file paths for torque sensors
-torque_txt_file_path = os.path.join(os.path.dirname(__file__), 'TP_V1.txt')
+torque_txt_file_path = os.path.join(os.path.dirname(__file__), 'TP_V2.txt')
 
 # Extracting data from the txt files for both torque sensors
 torque0_time = extract_elapsed_time(torque_txt_file_path)
@@ -28,30 +28,43 @@ torque1_time = create_linspace(torque1_time, torque1_voltage)
 print("New torque1 time: " + str(len(torque1_time)))
 
 # csv file paths for torque sensors
-torque_csv_file_path = 'C:\\Git\\GitHub\\MCS\\MC\\MC_state_machine_xADC\\Python\\datasorter\\LAB 03-12-24\\TP_V1.csv'
+torque_csv_file_path_CP1 = 'C:\Git\GitHub\MCS\MC\MC_state_machine_xADC\Python\datasorter\LAB 16-12-24\TP_V2C1.CSV'
+torque_csv_file_path_CP2 = 'C:\Git\GitHub\MCS\MC\MC_state_machine_xADC\Python\datasorter\LAB 16-12-24\TP_V2C2.CSV'
 
-time = 'x-axis'
-TP0_y = 'TP0'
-TP1_y = 'TP1'
+time = 'time'
+TP0_y = 'C1'
+TP1_y = 'C2'
 
 # Reading and scaling data from the csv files for both torque sensors
-torque0_x, torque0_y = read_csv(torque_csv_file_path, time, TP0_y)
+torque0_x, torque0_y = read_csv(torque_csv_file_path_CP1, time, TP0_y)
 torque0_x, torque0_y = csv_scaler(torque0_x, torque0_y, torque0_time)
 
-torque1_x, torque1_y = read_csv(torque_csv_file_path, time, TP1_y)
+torque1_x, torque1_y = read_csv(torque_csv_file_path_CP2, time, TP1_y)
 torque1_x, torque1_y = csv_scaler(torque1_x, torque1_y, torque1_time)
 
 # Adjusting the x values
-adder_TB0_x = +1.93 + 10
-adder_TB1_x = -1.68 + 10
+adder_TB0_x = 0.40+16.03-0.90
+adder_TB1_x = 0.40+16.03-0.90
 for i in range(len(torque0_x)):
     torque0_x[i] = torque0_x[i] + adder_TB0_x
 for i in range(len(torque1_x)):
     torque1_x[i] = torque1_x[i] + adder_TB1_x
 
+# # Plotting all the data together
+plt.figure()
+plt.plot(torque0_time, torque0_voltage, marker='o', linestyle='-', label='TP0 Sensor Voltage ADC')
+plt.plot(torque1_x, torque1_y, marker='o', linestyle='-', label='TP1 Sensor Voltage scope')
+plt.xlim([16, 28])
+plt.xlabel('Elapsed Time [s]')
+plt.ylabel('Sensor voltage [V]')
+plt.legend(['ADC voltage TP0', 'ADC voltage TP1'])
+plt.title('Voltage reading torque sensors')
+plt.grid()
+plt.show(block=False)
+
 # Adjusting the y values
-adder_TB0_y = + 1.68
-adder_TB1_y = -1.68
+adder_TB0_y = 0.15+2.472-1.154 
+adder_TB1_y = 0 
 for i in range(len(torque0_y)):
     torque0_y[i] = torque0_y[i] + adder_TB0_y
 for i in range(len(torque1_y)):
@@ -68,80 +81,51 @@ fig_tp0, (ax_tp0_1, ax_tp0_2) = plt.subplots(2, 1)
 ax_tp0_1.plot(torque0_x, torque0_y, marker='o', linestyle='-', label='TP0 Sensor Voltage scope')
 ax_tp0_1.plot(torque0_time, torque0_voltage, marker='o', linestyle='-', label='TP0 Sensor Voltage ADC')
 ax_tp0_1.set_title('TP0 Sensor Voltage over Time scope vs ADC')
-ax_tp0_1.set_xlabel('Elapsed Time [seconds]')
+ax_tp0_1.set_xlabel('Elapsed Time [s]')
 ax_tp0_1.set_ylabel('TP0 Sensor Voltage [V]')
-ax_tp0_1.set_xlim([12, 22])
+ax_tp0_1.set_xlim([16, 28])
 ax_tp0_1.legend()
 ax_tp0_1.grid(True)
+
+print(differences0[32:56+1])
+print("Average: " + str(sum(differences0[32:56+1])/len(differences0[32:56+1])))
 
 # Lower graph: Differences for TP0
 ax_tp0_2.plot(common_x0, differences0, marker='o', linestyle='-', label='Differences TP0')
 ax_tp0_2.set_title('Differences Between TP0 Datasets')
-ax_tp0_2.set_xlabel('Elapsed Time [seconds]')
+ax_tp0_2.set_xlabel('Elapsed Time [s]')
 ax_tp0_2.set_ylabel('Voltage Difference [V]')
-# ax_tp0_2.set_xlim([0, 10])
-# ax_tp0_2.set_ylim([-.5, .5])
+ax_tp0_2.set_xlim([16, 28])
+ax_tp0_2.set_ylim([-.2, .2])
 ax_tp0_2.legend()
 ax_tp0_2.grid(True)
-
 plt.tight_layout()
-# plt.show(block=False)
-plt.show()
+plt.show(block=False)
+
 
 
 # # Plotting the original datasets and differences for TP1
-# fig_tp1, (ax_tp1_1, ax_tp1_2) = plt.subplots(2, 1)
+fig_tp1, (ax_tp1_1, ax_tp1_2) = plt.subplots(2, 1)
 
-# # Upper graph: Original datasets for TP1
-# ax_tp1_1.plot(torque1_x, torque1_y, marker='o', linestyle='-', label='TP1 Sensor Voltage scope')
-# ax_tp1_1.plot(torque1_time, torque1_voltage, marker='o', linestyle='-', label='TP1 Sensor Voltage ADC')
-# ax_tp1_1.set_title('TP1 Sensor Voltage over Time scope vs ADC')
-# ax_tp1_1.set_xlabel('Elapsed Time [seconds]')
-# ax_tp1_1.set_ylabel('TP1 Sensor Voltage [V]')
-# ax_tp1_1.set_xlim([0, 10])
-# ax_tp1_1.legend()
-# ax_tp1_1.grid(True)
+# Upper graph: Original datasets for TP1
+ax_tp1_1.plot(torque1_x, torque1_y, marker='o', linestyle='-', label='TP1 Sensor Voltage scope')
+ax_tp1_1.plot(torque1_time, torque1_voltage, marker='o', linestyle='-', label='TP1 Sensor Voltage ADC')
+ax_tp1_1.set_title('TP1 Sensor Voltage over Time scope vs ADC')
+ax_tp1_1.set_xlabel('Elapsed Time [s]')
+ax_tp1_1.set_ylabel('TP1 Sensor Voltage [V]')
+ax_tp1_1.set_xlim([16, 28])
+ax_tp1_1.legend()
+ax_tp1_1.grid(True)
 
-# # Lower graph: Differences for TP1
-# ax_tp1_2.plot(common_x1, differences1, marker='o', linestyle='-', label='Differences TP1')
-# ax_tp1_2.set_title('Differences Between TP1 Datasets')
-# ax_tp1_2.set_xlabel('Elapsed Time [seconds]')
-# ax_tp1_2.set_ylabel('Voltage Difference [V]')
-# ax_tp1_2.set_xlim([0, 10])
-# ax_tp1_2.set_ylim([-.5, .5])
-# ax_tp1_2.legend()
-# ax_tp1_2.grid(True)
+# Lower graph: Differences for TP1
+ax_tp1_2.plot(common_x1, differences1, marker='o', linestyle='-', label='Differences TP1')
+ax_tp1_2.set_title('Differences Between TP1 Datasets')
+ax_tp1_2.set_xlabel('Elapsed Time [s]')
+ax_tp1_2.set_ylabel('Voltage Difference [V]')
+ax_tp1_2.set_xlim([16, 28])
+ax_tp1_2.set_ylim([-.2, .2])
+ax_tp1_2.legend()
+ax_tp1_2.grid(True)
 
-# plt.tight_layout()
-# plt.show()
-
-TP0_value = extract_TP0_sensor(torque_txt_file_path)
-
-
-plt.plot(TP0_value)
-plt.xlabel('Index')
-plt.ylabel('Sensor Value')
-plt.title('Sensor Values Over Time TP0')
+plt.tight_layout()
 plt.show()
-
-max_value = max(TP0_value)
-min_value = min(TP0_value)
-
-print(f"Maximum value: {max_value}")
-print(f"Minimum value: {min_value}")
-
-
-
-TP1_value = extract_TP1_sensor(torque_txt_file_path)
-plt.plot(TP1_value)
-plt.xlabel('Index')
-plt.ylabel('Sensor Value')
-plt.title('Sensor Values Over Time TP1')
-plt.show()
-
-max_value = max(TP1_value)
-min_value = min(TP1_value)
-
-print(f"Maximum value: {max_value}")
-print(f"Minimum value: {min_value}")
-
